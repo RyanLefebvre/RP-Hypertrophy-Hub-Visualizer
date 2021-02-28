749512e2a6a1b22f6d5bf19de52685e7710b95db
RyanLefebvre <ral1036@wildcats.unh.edu>
2021-02-28 12:19:12 -0500

    Completes exercise lists.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -158,7 +158,26 @@ const VOLUME_LANDMARKS: VolumeLandmark[] = [
     "Reps": "4-20",
     "RIR": "1-3",
     "url": "https://renaissanceperiodization.com/chest-training-tips-hypertrophy/",
-    "exercises": []
+    "exercises": [
+      "https://youtube.com/embed/4mfLHnFL0Uw",
+      "https://youtube.com/embed/Cj6P91eFXkM",
+      "https://youtube.com/embed/e_8HLu59-to",
+      "https://youtube.com/embed/_9VlfuYYC7w",
+      "https://youtube.com/embed/gmNlqsE3Onc",
+      "https://youtube.com/embed/YQ2s_Y7g5Qk",
+      "https://youtube.com/embed/JFm8KbhjibM",
+      "https://youtube.com/embed/BhlL-esnitU",
+      "https://youtube.com/embed/aV1U_mK3XOs",
+      "https://youtube.com/embed/0Wa9CfRXUkA",
+      "https://youtube.com/embed/GFYrRBoov3w",
+      "https://youtube.com/embed/8oR5hBwbIBc",
+      "https://youtube.com/embed/5CECBjd7HLQ",
+      "https://youtube.com/embed/lTfiohnjbyM",
+      "https://youtube.com/embed/TrTSvn5-MTk",
+      "https://youtube.com/embed/B09ZkYsnKko",
+      "https://youtube.com/embed/O-OBCfyh9Fw",
+      "https://youtube.com/embed/O5viuEPDXKY"
+    ]
   },
   {
     "Muscle": "Front-Delts",
@@ -216,7 +235,18 @@ const VOLUME_LANDMARKS: VolumeLandmark[] = [
     "Reps": "8-20",
     "RIR": "0-2",
     "url": "https://renaissanceperiodization.com/expert-advice/rear-delt-size-training-tips",
-    "exercises": []
+    "exercises": [
+      "https://youtube.com/embed/34gVHrkaiz0",
+      "https://youtube.com/embed/f0g5NkYiWUY",
+      "https://youtube.com/embed/-MODnZdnmAQ",
+      "https://youtube.com/embed/qz1OLup4W_M",
+      "https://youtube.com/embed/nzTY7j9ocR8",
+      "https://youtube.com/embed/90cE3rCLtmo",
+      "https://youtube.com/embed/z3PRz2aVA10",
+      "https://youtube.com/embed/8CGMuud1ANw",
+      "https://youtube.com/embed/5YK4bgzXDp0",
+      "https://youtube.com/embed/hPWYuhJMUhU"
+    ]
   },
   {
     "Muscle": "Biceps",
@@ -228,7 +258,24 @@ const VOLUME_LANDMARKS: VolumeLandmark[] = [
     "Reps": "8-15",
     "RIR": "0-2",
     "url": "https://renaissanceperiodization.com/bicep-training-tips-hypertrophy/",
-    "exercises": []
+    "exercises": [
+      "https://youtube.com/embed/iixND1P2lik",
+      "https://youtube.com/embed/pUS6HBQjRmc",
+      "https://youtube.com/embed/JnLFSFurrqQ",
+      "https://youtube.com/embed/yuozln3CC94",
+      "https://youtube.com/embed/opFVuRi_3b8",
+      "https://youtube.com/embed/fuK3nFvwgXk",
+      "https://youtube.com/embed/ke2shAeQ0O8",
+      "https://youtube.com/embed/tRXw8HQ7-oA",
+      "https://youtube.com/embed/cdmnvo3augg",
+      "https://youtube.com/embed/EK747VC37yE",
+      "https://youtube.com/embed/sxA__DoLsgo",
+      "https://youtube.com/embed/WG3vdcq__I0",
+      "https://youtube.com/embed/XOEL4MgekYE",
+      "https://youtube.com/embed/aTYlqC_JacQ",
+      "https://youtube.com/embed/Ja6ZlIDONac",
+      "https://youtube.com/embed/2CDKTFFp5fA"
+    ]
   },
   {
     "Muscle": "Triceps",
@@ -240,7 +287,25 @@ const VOLUME_LANDMARKS: VolumeLandmark[] = [
     "Reps": "8-20",
     "RIR": "0-2",
     "url": "https://renaissanceperiodization.com/triceps-hypertrophy-training-tips/",
-    "exercises": []
+    "exercises": [
+      "https://youtube.com/embed/q5X9thiKofE",
+      "https://youtube.com/embed/yZ83t4mrPrI",
+      "https://youtube.com/embed/l3rHYPtMUo8",
+      "https://youtube.com/embed/1u18yJELsh0",
+      "https://youtube.com/embed/Cp_bShvMY4c",
+      "https://youtube.com/embed/6Fzep104f0s",
+      "https://youtube.com/embed/4LA1kF7yCGo",
+      "https://youtube.com/embed/jPjhQ2hsAds",
+      "https://youtube.com/embed/IdZ7HXnatko",
+      "https://youtube.com/embed/1lrjpLuXH4w",
+      "https://youtube.com/embed/Bx8ga1BLHLE",
+      "https://youtube.com/embed/OChuGyCSC7U",
+      "https://youtube.com/embed/kqidUIf1eJE",
+      "https://youtube.com/embed/-xa-6cQaZKY",
+      "https://youtube.com/embed/gcRZqG8t44c",
+      "https://youtube.com/embed/Tih5iHyELsE",
+      "https://youtube.com/embed/ktU2H0DDmwk"
+    ]
   },
   {
     "Muscle": "Calves",
@@ -252,7 +317,13 @@ const VOLUME_LANDMARKS: VolumeLandmark[] = [
     "Reps": "8-20",
     "RIR": "0-2",
     "url": "https://renaissanceperiodization.com/calves-training-tips-hypertrophy/",
-    "exercises": []
+    "exercises": [
+      "https://youtube.com/embed/N3awlEyTY98",
+      "https://youtube.com/embed/__qfDhdByMY",
+      "https://youtube.com/embed/_gEx2ijsmNM",
+      "https://youtube.com/embed/hh5516HCu4k",
+      "https://youtube.com/embed/KxEYX_cuesM"
+    ]
   },
   {
     "Muscle": "Abs",
@@ -264,7 +335,16 @@ const VOLUME_LANDMARKS: VolumeLandmark[] = [
     "Reps": "8-20",
     "RIR": "0-2",
     "url": "https://renaissanceperiodization.com/ab-training/",
-    "exercises": []
+    "exercises": [
+      "https://youtube.com/embed/RD_A-Z15ER4",
+      "https://youtube.com/embed/7FwGZ8qY5OU",
+      "https://youtube.com/embed/-OUSBPnHvsQ",
+      "https://youtube.com/embed/T_X5rb3G5lk",
+      "https://youtube.com/embed/pXg8qppif7I",
+      "https://youtube.com/embed/6GMKPQVERzw",
+      "https://youtube.com/embed/DAnTf16NcT0",
+      "https://youtube.com/embed/BIOM5eSsJ_8"
+    ]
   },
   {
     "Muscle": "Traps",
@@ -276,7 +356,18 @@ const VOLUME_LANDMARKS: VolumeLandmark[] = [
     "Reps": "8-20",
     "RIR": "0-2",
     "url": "https://renaissanceperiodization.com/trap-training-tips-hypertrophy/",
-    "exercises": []
+    "exercises": [
+      "https://youtube.com/embed/M_MjF5Nm_h4",
+      "https://youtube.com/embed/d9daNDIXtK8",
+      "https://youtube.com/embed/_t3lrPI6Ns4",
+      "https://youtube.com/embed/5z7ZtboxbBY",
+      "https://youtube.com/embed/GH_l85Ky3vA",
+      "https://youtube.com/embed/BeIcUXQ3RDc",
+      "https://youtube.com/embed/2zaT3WAgZi0",
+      "https://youtube.com/embed/zgToz5FiI-E",
+      "https://youtube.com/embed/YykmcX2b-LY",
+      "https://youtube.com/embed/nOn_Bz0zrwQ"
+    ]
   },
   {
     "Muscle": "Forearms",
@@ -288,7 +379,12 @@ const VOLUME_LANDMARKS: VolumeLandmark[] = [
     "Reps": "8-20",
     "RIR": "0-2",
     "url": "https://renaissanceperiodization.com/expert-advice/forearm-growth-training-tips",
-    "exercises": []
+    "exercises": [
+      "https://youtube.com/embed/lfQR7oVS8eo",
+      "https://youtube.com/embed/iQ4JjOK73PE",
+      "https://youtube.com/embed/2wPpcJBe03o",
+      "https://youtube.com/embed/WVAaKJvToe0"
+    ]
   }
 ];
 
@@ -474,6 +570,8 @@ export class DashboardComponent implements OnInit, OnDestroy {
           });
           if (muscleGroupObject) {
             this.setSelectedMuscleGroup(muscleGroupObject)
+          } else {
+            this.goBackToDashBoard();
           }
         }
       }
